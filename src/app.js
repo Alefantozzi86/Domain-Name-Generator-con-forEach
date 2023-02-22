5b016a11b06fe380d6232267cec750a032b39502
Alefantozzi86 <alefantozzi86@gmail.com>
2023-02-22 21:15:12 +0000

    CambiosRealizados

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let newDomain = [];
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+  const domain = [".com", ".net", ".es"];
+
+  function getDomain() {
+    pronoun.forEach(element1 => {
+      adj.forEach(element2 => {
+        noun.forEach(element3 => {
+          domain.forEach(element4 => {
+            //console.log(`${element1}${element2}${element3}${element4}`);
+            let div = document.createElement("div");
+            div.innerText = `
+          ${element1}${element2}${element3}${element4}
+          `;
+            document.body.appendChild(div);
+          });
+        });
+      });
+    });
+  }
+  getDomain();
 };
